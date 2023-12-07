ec8f63e0dfe8671f34251b81cf66b10260e2cf2f
sniro <snirosh12@gmail.com>
2023-12-07 15:07:00 +0530

    constant moved to separate file

---
+++ src/constants/utilConstant.js
@@ -0,0 +1,14 @@
+//cacheUtils
+export const WEATHER_DATA_TIME_STAMP_CACHE_LABEL = "weatherDataTimestamp";
+
+//loadTimeDataUtils
+export const NUMERIC_LABEL = "numeric";
+export const SHORT_LABEL = "short";
+export const LOCALE_EN_US_LABEL = "en-US";
+
+//getCardBgColour
+export const BLUE = "bg-blue-500";
+export const PURPLE = "bg-purple-500";
+export const GREEN = "bg-green-500";
+export const ORANGE = "bg-orange-500";
+export const RED = "bg-red-500";
